c81029f87d1a67a6a23472a7fcf0efb85646bed9
zangwang <info@zangwang.tw>
2024-01-01 21:34:48 +0800

    Modify package.json homepage url, add #

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
 import {
   createHashRouter,
   RouterProvider,
 } from "react-router-dom";
+
+import App from './App.jsx'
+import './index.css'
+
+
 
 const router = createHashRouter([
   {
